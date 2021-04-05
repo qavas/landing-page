6b8eb760fcbf82773c463adc328eff26310d3878
Qavas <58612903+qavas@users.noreply.github.com>
2021-04-05 14:01:04 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,38 @@
+var config = {
+
+    // main section
+    websitetitle: "Title",
+    title: "Name or Title",
+    description: "Description or Portfolio",
+
+    // socials - true for yes, false for no
+        instagram: false,
+        github: true,
+        discord: true,
+        twitter: false,
+		youtube: false,
+		twitch: false,
+		snapchat: false,
+
+    // social links
+
+        instagramlink: "https://instagram.com",
+        githublink: "https://github.com",
+		discordlink: "https://discord.com",
+        twitterlink: "https://twitter.com",
+		youtubelink: "https://youtube.com",
+		twitchlink: "https://twitch.tv",
+		snapchatlink: "https://snapchat.com",
+		
+    // images section - set to false if you dont want a logo.
+
+        logo: true, // My logo is in by default, please add your own :)
+        logopath: "images/logo.png",
+        backgroundpath: "images/background.gif",
+
+    //colors
+        textcolor: "white", // hex or "black, white, yellow, blue, red, green, orange, etc"
+        iconcolor: "#621ed1", // hex or "black, white, yellow, blue, red, green, orange, etc"
+
+    //font - not in config. If you'd like to change the font, contact qavas#0746 on discord.
+}
